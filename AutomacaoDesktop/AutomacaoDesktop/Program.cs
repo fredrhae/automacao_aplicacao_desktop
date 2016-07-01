e10bb7c6e71e3cf3cde6596d3c007767859ea00c
Frederico Rhae <fredrhae@gmail.com>
2016-07-01 00:39:27 -0300

    Primeiro commit do projeto
    Primeira versão da aplicação de automação desktop. Inicialmente, só fiz
    alguns pequenos testes para tentar iniciar um aplicativo word, escrever
    texto nele e em seguida salvar.
    
    Agora é refatorar para criar as classes de acordo com o que deve ser
    feito.

---
+++ AutomacaoDesktop/AutomacaoDesktop/Program.cs
@@ -0,0 +1,32 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using System;
+
+
+namespace AutomacaoDesktop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dc = new DesiredCapabilities();
+            dc.SetCapability("app", @"C:/Program Files (x86)/Microsoft Office/Office14/WINWORD.EXE");
+            var driver = new RemoteWebDriver(new Uri("http://localhost:9999"), dc);
+
+            var textField = driver.FindElementByClassName("_WwG");
+            textField.Click();
+            textField.SendKeys("Alô alô testando");
+
+            var saveButton = driver.FindElementByName("Save");
+            saveButton.Click();
+
+            var savePopup = driver.FindElementByName("Save As");
+            var fileName = savePopup.FindElement(By.Id("1001"));
+            fileName.Click();
+            fileName.SendKeys("nome_qualquer");
+            savePopup.FindElement(By.Name("Save")).Click();
+
+            driver.Quit();
+        }
+    }
+}
